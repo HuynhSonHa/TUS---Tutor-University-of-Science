26e21425505527e877c887a5d7b2ea8e685c3df7
thduy161103 <147958221+thduy161103@users.noreply.github.com>
2023-11-23 18:51:35 +0700

    Connect database

--- src/server.js
+++ src/server.js
@@ -1,17 +1,25 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const methodOverride = require('method-override');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const db = require('./config/dbMongo');
+
+// Connect to DB
+db.connect();
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+//dùng để chỉnh form từ post sang put
+app.use(methodOverride('_method'));
 
 app.use(logger('dev'));
 app.use(express.json());
