3d5d99c75b34dc9385c85bbb2817c09151122518
SonHa <hsh261003@gmail.com>
2023-11-28 08:37:47 +0700

    routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
-var express = require('express');
-var router = express.Router();
+const meRouter = require("./me");
+const newsRouter = require("./news");
+const siteRouter = require("./site");
+const courseRouter = require("./courses");
+const loginRouter = require("./login");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('home/home');
-});
+function route(app) {
+  //   app.get("/news", function (req, res) {
+  //     res.render("news");
+  //   });
+  app.use("/news", newsRouter);
 
-module.exports = router;
+  //   app.get("/search", function (req, res) {
+  //     console.log(req.query.q);
+  //     res.render("search");
+  //   });
+
+  app.post("/search", function (req, res) {
+    console.log(req.body);
+    res.send("");
+  });
+  //   app.get("/", function (req, res) {
+  //     res.render("home");
+  //   });
+  app.use("/me", meRouter);
+  app.use("/courses", courseRouter);
+  app.use("/home", siteRouter);
+  app.use("/", loginRouter);
+}
+
+module.exports = route;
