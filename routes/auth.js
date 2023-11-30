0740a50f76b1d4a08382ba78bbe0d0a605cd1e3b
thduy161103 <147958221+thduy161103@users.noreply.github.com>
2023-11-30 15:47:07 +0700

    Use passport local

--- routes/auth.js
+++ routes/auth.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const {getSignIn,getSignUp,postSignIn,postSignUp,getHomePage} = require("../controllers/AuthController");
 
 router.get("/signin", getSignIn);
+router.get("/login", getSignIn);
 router.get("/signup", getSignUp);
 router.get("/register", getSignUp);
 router.post("/signin", postSignIn);
+router.post("/login", postSignIn);
 router.post("/signup", postSignUp);
 router.post("/register", postSignUp);
 //router.get("/forget-password", authController.forgetpassword);
