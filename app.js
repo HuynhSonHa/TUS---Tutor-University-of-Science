4eb674b97ce790abcd0b745ae8e15c7431911867
PUPMKINN <93564591+PUPMKINN@users.noreply.github.com>
2023-12-13 09:19:27 +0700

    css

--- app.js
+++ app.js
@@ -62,5 +62,11 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+const PORT = 3001; // or any other available port
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
 module.exports = app;
 
