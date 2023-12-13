9fc4b695ad055c4d979ee47a946e94d1f9eb95ae
PUPMKINN <93564591+PUPMKINN@users.noreply.github.com>
2023-12-13 08:43:06 +0700

    sss

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -9,7 +9,6 @@ const getHomePage = (req, res, next) => {
 
 //[GET] /signin
 const getSignIn = (req, res, next) => {
-=======
 const { check, validationResult } = require('express-validator');
 const passport = require('passport');
 const User = require('../models/User');
@@ -27,7 +26,6 @@ exports.getSignIn = function (req, res, next) {
     hasErrors: messages.length > 0,
   });
 };
-<<<<<<< Updated upstream
 
 //[POST] /signin
 const postSignIn = (req, res, next) => {
@@ -40,37 +38,12 @@ const postSignIn = (req, res, next) => {
 
 //[GET] /signup
 const getSignUp = (req, res, next) => {
-=======
-//[POST] /signin
-exports.postSignIn = function(req, res, next){
-  User.findOne({ 'username': req.body.username })
-  .then((user) => {
-    if (!user) {
-      res.redirect("login/signin")
-      res.status(400).json({ error: 'Not user found' });
-    }
-    else if (!user.validPassword(req.body.password)) {
-      res.redirect("login/signin")
-      res.status(400).json({ error: 'Wrong password' });
-    }
-    else {
-      res.redirect("/home");
-      //res.status(200).json({ message: 'Sign in successfully' });
-    }
-  })
-  .catch(next);
-};
-
-//[GET] /signup
-exports.getSignUp = function (req, res, next) {
->>>>>>> Stashed changes
   var messages = req.flash('error');
   res.render('login/signup', {
     messages: messages,
     hasErrors: messages.length > 0,
   });
 };
-<<<<<<< Updated upstream
 
 // [POST] /signup
 const postSignUp = (req, res, next) => {
@@ -116,31 +89,3 @@ module.exports = {
   postSignUp,
 }
 
-=======
-// [POST] /signup
-exports.postSignUp = function(req, res, next) {
-  User.findOne({ 'username': req.body.username })
-  .then( (user) => {
-    if (user) {
-      res.redirect("login/signup");
-      return res.json({ error: 'Username is already in use.' });
-    }
-    var newUser = new User();
-    newUser.username = req.body.username;
-    newUser.email = req.body.email;
-    newUser.password = newUser.encryptPassword(req.body.password);
-    newUser.save()
-    .then(() => {
-      res.redirect('login/signin');
-      res.json({ message: 'User registered successfully' });
-    })
-    .catch((err) => {
-      console.error(err);
-      res.redirect("login/signup");
-      res.json({ error: 'Internal Server Error' });
-    });
-  })
-  .catch(next);
-};
-//res.session.user = user;
->>>>>>> Stashed changes
