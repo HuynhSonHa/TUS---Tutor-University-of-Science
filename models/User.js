3e7f0e8893ba0c95c988460fd3ad0cbcb8fcce90
thduy161103 <147958221+thduy161103@users.noreply.github.com>
2023-12-17 20:28:24 +0700

    Debug

--- models/User.js
+++ models/User.js
@@ -2,6 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const bcrypt = require("bcryptjs")
 const UserScheme = new Schema({
+    fullname: {
+        type: String,
+        default: "fullname",
+    },
     username: {
         type: String,
         required: [true, 'Please provide username'],
