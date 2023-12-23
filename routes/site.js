13ffb922869aaba4aa47e003528e209ea7e8a7ef
thduy161103 <147958221+thduy161103@users.noreply.github.com>
2023-12-23 15:35:04 +0700

    Test

--- routes/site.js
+++ routes/site.js
@@ -4,6 +4,10 @@ const router = express.Router();
 const siteController = require("../controllers/SiteController");
 
 router.get("/search", siteController.search);
+router.get("/test", (req, res, next) => {
+    res.render("index", {layout: 'admin'});
+    //res.render();
+})
 router.get("/", siteController.getHomePage);
 
 module.exports = router;
