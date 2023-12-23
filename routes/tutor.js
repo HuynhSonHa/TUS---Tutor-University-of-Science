ca0780f44a34065507e8d3bace9d87f5231988d2
thduy161103 <147958221+thduy161103@users.noreply.github.com>
2023-12-23 09:09:11 +0700

    Modify profile tutor

--- routes/tutor.js
+++ routes/tutor.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const multer = require('multer');
 const router = express.Router();
 const tutorController = require('../controllers/TutorController');
 const {isTutor} = require('../middlewares/isAuthenticated');
+const storage = require('../config/multer');
+const upload = multer({ storage: storage });
 
 
 router.get('/stored/courses', isTutor, tutorController.storedCourses);
@@ -9,7 +12,7 @@ router.get('/stored/waiting-list', isTutor, tutorController.storedStudents);
 router.get('/create', isTutor, tutorController.createCourse);
 router.post('/createNewCourse', isTutor, tutorController.createNewCourse);
 router.get('/profile', isTutor, tutorController.profile);
-router.post('/profile', isTutor, tutorController.editProfile);
+router.post('/profile', isTutor, upload.single('avatar'), tutorController.editProfile);
 router.get('/tutor-mode', tutorController.getTutorMode);
 router.get('/waitingStudent/:id', tutorController.getDetailStudent);
 router.get("/accepted/:id", tutorController.acceptStudent);
